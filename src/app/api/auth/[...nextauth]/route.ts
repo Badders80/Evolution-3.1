165c41ca568260af6bfd5773c232edb268fb15b0
Alex Baddeley <alex@evolutionstables.nz>
2026-01-09 00:12:32 +1300

    Remove session user id assignment

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -47,9 +47,6 @@ const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub!
-      }
       return session
     },
   },
